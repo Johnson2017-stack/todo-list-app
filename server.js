961e5b823931d512361add2ca379772f95b384b8
Andrew Johnson <132528189+Johnson2017-stack@users.noreply.github.com>
2023-06-25 20:20:56 -0700

    added a js file for edit, dlete and render options for the todo list, conected the style correctly, setup sever correctly

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express')
-
 const server = express();
 const PORT = 3000;
+
+server.use(express.static("public"));
 
 server.set('views', './src/views');
 
 server.set('view engine', 'ejs');
 
 server.get("/", (req, res) => {
-        res.send(index.js);
+        res.render('index');
       });
-
-
 
 server.listen(PORT, () => {
     console.log(`Listening on, ${PORT}`);
   });
+
+
+  
